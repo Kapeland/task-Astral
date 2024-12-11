29b11de2e9fabf50c3377bb20cc32cec681d80af
Kapeland <mdidkovskij@gmail.com>
2024-12-11 12:43:17 +0300

    Refactored logger. Now it uses slog.

--- internal/utils/logger/logger.go
+++ internal/utils/logger/logger.go
@@ -1,24 +1,93 @@
 package logger
 
 import (
-	"fmt"
-	"time"
+	"github.com/Kapeland/task-Astral/internal/utils/config"
+	slogmulti "github.com/samber/slog-multi"
+	slogsampling "github.com/samber/slog-sampling"
+	"log/slog"
+	"os"
+	"strings"
 )
 
-const logTimeLayout = "2006-01-02 15:04:05.00"
+var logger *Logger
 
-const (
-	InfoPrefix  = "INFO"
-	DebugPrefix = "DEBUG"
-	WarnPrefix  = "WARN"
-	ErrPrefix   = "ERROR"
-)
+func GetLogger() Logger {
+	if logger != nil {
+		return *logger
+	}
 
-func Log(prefix string, message string) {
-	out := createLogMessage(time.Now(), prefix, message)
-	fmt.Println(out)
+	panic("Create logger first.")
 }
 
-func createLogMessage(time1 time.Time, prefix string, message string) string {
-	return fmt.Sprintf("%s\t%s\t%s", time1.Format(logTimeLayout), prefix, message)
+type Logger struct {
+	Logger   *slog.Logger
+	logLevel *slog.LevelVar
+}
+
+func (lgr Logger) Info(msg, tp, method, after string) {
+	lgr.Logger.Info(msg, slog.String("type", tp), slog.String("method", method), slog.String("after", after))
+	return
+}
+
+func (lgr Logger) Debug(msg, tp, method, after string) {
+	lgr.Logger.Debug(msg, slog.String("type", tp), slog.String("method", method), slog.String("after", after))
+	return
+}
+
+func (lgr Logger) Warn(msg, tp, method, after string) {
+	lgr.Logger.Warn(msg, slog.String("type", tp), slog.String("method", method), slog.String("after", after))
+	return
+}
+
+func (lgr Logger) Error(msg, tp, method, after string) {
+	lgr.Logger.Error(msg, slog.String("type", tp), slog.String("method", method), slog.String("after", after))
+	return
+}
+
+func CreateLogger(cfg *config.Config) Logger {
+	samplingOption := slogsampling.UniformSamplingOption{
+		Rate: cfg.Logger.LogRate,
+	}
+
+	logLevel := &slog.LevelVar{} // INFO
+
+	switch strings.ToLower(cfg.Logger.Lvl) {
+	case "info":
+		logLevel.Set(slog.LevelInfo)
+	case "debug":
+		logLevel.Set(slog.LevelDebug)
+	case "warn":
+		logLevel.Set(slog.LevelWarn)
+	case "error":
+		logLevel.Set(slog.LevelError)
+	}
+
+	opts := &slog.HandlerOptions{
+		Level:     logLevel,
+		AddSource: true,
+	}
+
+	var handler slog.Handler = slog.NewTextHandler(os.Stdout, opts)
+
+	if !cfg.Project.Debug {
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+		opts.AddSource = false
+	}
+	logger = &Logger{
+		Logger: slog.New(
+			slogmulti.
+				Pipe(samplingOption.NewMiddleware()).
+				Handler(handler),
+		),
+		logLevel: logLevel,
+	}
+
+	return *logger
+}
+
+// SetLogLvl allows to set log lvl in runtime
+func SetLogLvl(lvl slog.Level) {
+	if logger != nil {
+		logger.logLevel.Set(lvl)
+	}
 }
