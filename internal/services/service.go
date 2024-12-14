a8c46ac1a4929aadd51652a68bc8da5131491437
Kapeland <mdidkovskij@gmail.com>
2024-12-14 17:24:18 +0300

    Structure refactoring and added new middleware.

--- internal/services/service.go
+++ internal/services/service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	mw "github.com/Kapeland/task-Astral/internal/services/middleware"
+	"github.com/Kapeland/task-Astral/internal/services/servers"
 	"github.com/Kapeland/task-Astral/internal/utils/config"
 	"github.com/Kapeland/task-Astral/internal/utils/logger"
 	"github.com/chenyahui/gin-cache"
@@ -11,18 +13,18 @@ import (
 )
 
 type Service struct {
-	fm FileModelManager
-	am AuthModelManager
+	fm servers.FileModelManager
+	am servers.AuthModelManager
 	um UsersModelManager
 }
 
-func NewService(fm FileModelManager, am AuthModelManager, um UsersModelManager) Service {
+func NewService(fm servers.FileModelManager, am servers.AuthModelManager, um UsersModelManager) Service {
 	return Service{fm: fm, am: am, um: um}
 }
 
 func (s Service) Launch(cfg *config.Config, lgr *logger.Logger) {
-	implAuth := authServer{s.am}
-	implFile := fileServer{s.fm, s.am}
+	implAuth := servers.AuthServer{A: s.am}
+	implFile := servers.FileServer{F: s.fm, A: s.am}
 
 	memoryStore := persist.NewMemoryStore(2 * time.Minute)
 	if !cfg.Project.Debug {
@@ -36,34 +38,25 @@ func (s Service) Launch(cfg *config.Config, lgr *logger.Logger) {
 
 	usrGr := router.Group("/api")
 	{
-		usrGr.POST("/register", CachePurge(memoryStore, lgr), implAuth.Register)
+		usrGr.POST("/register", mw.CachePurge(memoryStore, lgr), implAuth.Register)
 	}
 	authGR := router.Group("/api")
 	{
-		authGR.POST("/auth", CachePurge(memoryStore, lgr), implAuth.Auth)
-		authGR.DELETE("/auth/:token", CachePurge(memoryStore, lgr), implAuth.Logout)
+		authGR.POST("/auth", mw.CachePurge(memoryStore, lgr), implAuth.Auth)
+		authGR.DELETE("/auth/:token", mw.CachePurge(memoryStore, lgr), implAuth.Logout)
 	}
 
 	docsGr := router.Group("/api")
 	{
-		docsGr.POST("/docs", CachePurge(memoryStore, lgr), implFile.UploadDoc)
-		docsGr.GET("/docs", cache.CacheByRequestURI(memoryStore, 2*time.Minute), implFile.GetDocsList)
-		docsGr.HEAD("/docs", cache.CacheByRequestURI(memoryStore, 2*time.Minute), implFile.GetDocsList)
-		docsGr.GET("/docs/:id", cache.CacheByRequestURI(memoryStore, 2*time.Minute), implFile.GetDoc)
-		docsGr.HEAD("/docs/:id", cache.CacheByRequestURI(memoryStore, 2*time.Minute), implFile.GetDoc)
-		docsGr.DELETE("/docs/:id", CachePurge(memoryStore, lgr), implFile.DeleteDoc)
+		docsGr.POST("/docs", mw.ValidateTokenInMultipartFrom(s.am, lgr), mw.CachePurge(memoryStore, lgr), implFile.UploadDoc)
+		docsGr.GET("/docs", cache.CacheByRequestURI(memoryStore, 2*time.Minute), mw.ValidateTokenInQuery(s.am, lgr), implFile.GetDocsList)
+		docsGr.HEAD("/docs", cache.CacheByRequestURI(memoryStore, 2*time.Minute), mw.ValidateTokenInQuery(s.am, lgr), implFile.GetDocsList)
+		docsGr.GET("/docs/:id", cache.CacheByRequestURI(memoryStore, 2*time.Minute), mw.ValidateTokenInQuery(s.am, lgr), implFile.GetDoc)
+		docsGr.HEAD("/docs/:id", cache.CacheByRequestURI(memoryStore, 2*time.Minute), mw.ValidateTokenInQuery(s.am, lgr), implFile.GetDoc)
+		docsGr.DELETE("/docs/:id", mw.ValidateTokenInQuery(s.am, lgr), mw.CachePurge(memoryStore, lgr), implFile.DeleteDoc)
 	}
 
 	if err := router.Run(":" + strconv.Itoa(cfg.Rest.Port)); err != nil {
 		lgr.Error(err.Error(), "Service", "Launch", "router.Run")
-	}
-}
-
-func CachePurge(store *persist.MemoryStore, lgr *logger.Logger) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		err := store.Cache.Purge()
-		if err != nil {
-			lgr.Error(err.Error(), "Service", "CachePurge", "Purge")
-		}
 	}
 }
